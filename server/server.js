8a8ae69ba91518847da8c20c0d9ee471152ed195
jose.aspmiralles <jose.aspmiralles@sp.senac.br>
2018-11-27 11:37:38 -0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -26,6 +27,26 @@ app.get('/todos', (req, res) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
+  });
+});
+
+// GET /todos/12345667
+app.get('/todos/:id', (req, res) => {  
+  var id = req.params.id;
+
+  // valid id
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (todo) {
+      res.send({todo});
+    } else {
+      res.status(404).send();
+    }
+  }, (e) => {
+    res.status(400).send();
   });
 });
 
