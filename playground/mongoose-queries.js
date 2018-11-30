fe4ae9c43df204be4984ec7f17dae601c29ec369
jose.aspmiralles <jose.aspmiralles@sp.senac.br>
2018-11-30 14:07:43 -0200

    Test the DELETE /todos/:id route

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -3,42 +3,16 @@ const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
 const {User} = require('./../server/models/user');
 
-/*var id = '5bfbfa48a0dcf943689f80f611';
-
-if (!ObjectID.isValid(id)) {
-    return console.log("Id not valid");
-}*/
-
-/*Todo.find({
-    _id: id
-}).then((todos) => {
-    console.log('Todos', todos);
+Todo.remove({}).then((result) => {
+    console.log(result);
 });
 
-Todo.findOne({
-    _id: id
-}).then((todo) => {
-    console.log('Todo', todo);
-});*/
+//
+Todo.findOneAndRemove().then((result) => {
+    console.log(result);
+});
 
-/*Todo.findById(id).then((todo) => {
-    if (!todo) {
-        return console.log('Id not found');
-    }
-
-    console.log('Todo By Id', todo);
-}).catch((e) => console.log(e));*/
-
-var userId = '5bb26a3d2ad8531c44a25138';
-
-if (!ObjectID.isValid(userId)) {
-    return console.log("userId not valid");
-}
-
-User.findById(userId).then((user) => {
-    if (!user) {
-        return console.log('Id not found');
-    }
-
-    console.log('User By Id', user);
-}).catch((e) => console.log(e));
+//
+Todo.findByIdAndRemove(id).then((result) => {
+    console.log(result);
+});
